c06a35513a8a2ef1fe8d05c0a2c7a97407703295
keik <k4t0.kei@gmail.com>
2014-12-02 13:07:19 +0900

    Fix bad preconditions of tests

--- src/test/java/info/keik/bookmanager/service/BooksServiceTest.java
+++ src/test/java/info/keik/bookmanager/service/BooksServiceTest.java
@@ -17,48 +17,55 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
+@TransactionConfiguration(defaultRollback = true)
 public class BooksServiceTest extends
-        AbstractTransactionalJUnit4SpringContextTests {
+    AbstractTransactionalJUnit4SpringContextTests {
 
     @Autowired
     BooksService sut;
 
     @Before
     public void setup() {
-        deleteFromTables("books");
+        // deleteFromTables("books");
+        // deleteFromTables("tags");
+        // deleteFromTables("books_tags");
     }
 
     @Test
     public void 本を追加できる() {
         // setup
+        int count = sut.findAllBooks().size();
         // exercise
         sut.addBook(new Book("aaa1", "bbb1", "ccc1"));
         // verify
-        assertThat(sut.findAllBooks(), is(hasSize(1)));
+        assertThat(sut.findAllBooks(), is(hasSize(count + 1)));
         // teardown
     }
 
     @Test
     public void IDで指定した本を削除できる() {
         // setup
+        int count = sut.findAllBooks().size();
         Book book1 = new Book("aaa1", "bbb1", "ccc1");
         sut.addBook(book1);
         sut.addBook(new Book("aaa2", "bbb2", "ccc2"));
         // exercise
         sut.deleteBook(book1.getId());
         // verify
-        assertThat(sut.findAllBooks(), is(hasSize(1)));
+        assertThat(sut.findAllBooks(), is(hasSize(count + 1)));
         // teardown
     }
 
     @Test
     public void 複数のIDで指定した本を削除できる() {
         // setup
+        int count = sut.findAllBooks().size();
         Book book1 = new Book("aaa1", "bbb1", "ccc1");
         sut.addBook(book1);
         Book book2 = new Book("aaa2", "bbb2", "ccc2");
@@ -71,7 +78,7 @@ public class BooksServiceTest extends
         // exercise
         sut.deleteBooks(ids);
         // verify
-        assertThat(sut.findAllBooks(), is(hasSize(1)));
+        assertThat(sut.findAllBooks(), is(hasSize(count + 1)));
         // teardown
     }
 
@@ -81,27 +88,28 @@ public class BooksServiceTest extends
         Book book1 = new Book("aaa1", "bbb1", "ccc1");
         sut.addBook(book1);
         assertThat(sut.findBookById(book1.getId()),
-                is(samePropertyValuesAs(book1)));
+            is(samePropertyValuesAs(book1)));
         // exercise
         Book newbook = new Book("xxx1", "yyy1", "zzz1");
         newbook.setId(book1.getId());
         sut.updateBook(newbook);
         // verify
         assertThat(sut.findBookById(book1.getId()),
-                is(samePropertyValuesAs(newbook)));
+            is(samePropertyValuesAs(newbook)));
         // teardown
     }
 
     @Test
     public void 全ての本を取得できる() {
         // setup
+        int count = sut.findAllBooks().size();
         sut.addBook(new Book("aaa1", "bbb1", "ccc1"));
         sut.addBook(new Book("aaa2", "bbb2", "ccc2"));
         sut.addBook(new Book("aaa3", "bbb3", "ccc3"));
         sut.addBook(new Book("aaa4", "bbb4", "ccc4"));
         // exercise
         // verify
-        assertThat(sut.findAllBooks().size(), is(Integer.valueOf(4)));
+        assertThat(sut.findAllBooks(), hasSize(count + 4));
         // teardown
     }
 
