591633e183e44db0d7c00c6bffcf36255f9865d6
keik <k4t0.kei@gmail.com>
2014-12-07 19:11:03 +0900

    Rename class name correctly

--- src/main/java/info/keik/bookmanager/domain/RentalsRepository.java
+++ src/main/java/info/keik/bookmanager/domain/RentalsRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RentlsRepository extends JpaRepository<Stock, Integer> {
+public interface RentalsRepository extends JpaRepository<Stock, Integer> {
 
 }
